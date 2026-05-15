5cd04403ed155a9f0bc4ddf8f13f942d97d60079
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-05-16 01:32:17 +0530

    find prime

---
+++ FindPrime.java
@@ -0,0 +1,21 @@
+/*
+check number is prime Or not
+*/
+import java.util.*;
+public class FindPrime{
+	public static void main(String...x){
+		System.out.println("Enter the number to check prime");
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		System.out.println(checkPrime(num));
+	}
+	public static boolean checkPrime(int num){
+		if(num <1) return false;
+		if(num >= 1 && num <= 3) return true;
+		
+		for(int i =2; i < Math.sqrt(num); i++){
+			if(num % i ==0) return false;
+		}
+		return true;
+	}
+}
