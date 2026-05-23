bedb39f94ddb391d8ba80b7eb1da9083f3fa5073
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-05-23 21:39:53 +0530

    prime number

--- FindPrime.java
+++ FindPrime.java
@@ -13,11 +13,11 @@ public class FindPrime{
 	}
 	public static boolean checkPrime(int num){
 		if(num <1) return false;
-		if(num >= 1 && num <= 3) return true;
+		if(num >= 1 && num <= 3) return true;	
+		
 		for(int i =2; i < Math.sqrt(num); i++){
 			if(num % i ==0) return false;
 		}
-		
 		return true;
 	}
 }
