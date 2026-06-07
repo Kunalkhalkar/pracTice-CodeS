c789c536b45c4648776f7636fa50b588dd9ba71a
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-06-07 23:11:18 +0530

    Update LinkedList3.java

--- LinkedList3.java
+++ LinkedList3.java
@@ -5,9 +5,7 @@ public class LinkedList22{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		Linkedlist ll = new Linkedlist();
 		String str;
-		
 		while(true){
-			
 			System.out.println("Enter the data in Linked List");
 			str = br.readLine();
 			
